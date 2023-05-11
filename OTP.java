e30b5aa056eaf9ddce38406b93c0174c82f2d6ad
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-05-11 13:19:47 +0300

    OTP function works well

--- OTP.java
+++ OTP.java
@@ -1,19 +1,41 @@
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.util.Random;
 
-public class SendOTP {
+public class OTP {
 
-    public static void main(String[] args) {
+    public String generateOTP() {
+
+        // Characters allowed in the OTP
+        String allowedChars = "0123456789";
+
+        // Random object
+        Random random = new Random();
+
+        // StringBuilder to store the OTP
+        StringBuilder otp = new StringBuilder();
+
+        // Generate random OTP
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(allowedChars.length());
+            otp.append(allowedChars.charAt(index));
+        }
+
+        // Return the OTP as a string
+        return otp.toString();
+    }
+
+    public void sendOTP(String userEmail, String OTP) {
         // Email details
         String host = "smtp.gmail.com";
         String port = "587";
-        String username = "asaney555@gmail.com"; // replace with your email
-        String password = "vkp8get3cvm0FJT!uwt"; // replace with your email password
+        String username = "asmaaiheikal@gmail.com"; // replace with your email
+        String password = "ddvmragbrfguvtnf"; // replace with your email password
 
         // User details
-        String userEmail = "asmaaiheikal@gmail.com"; // replace with user's email
-        String OTP = "123456"; // replace with generated OTP
+//        String userEmail = "asaney555@gmail.com"; // replace with user's email
+//        String OTP = "123456"; // replace with generated OTP
 
         // Email properties
         Properties props = new Properties();
--- User.java
+++ User.java
@@ -1,11 +1,14 @@
+import java.util.Objects;
 import java.util.regex.*;
 import java.util.Scanner;
 import java.io.*;
+
 public class User {
 
     private String email;
     private String password;
     private String address;
+
     public boolean isValidEmail(String email) {
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
                 "[a-zA-Z0-9_+&*-]+)*@" +
@@ -14,6 +17,7 @@ public class User {
         Pattern pattern = Pattern.compile(emailRegex);
         return pattern.matcher(email).matches();
     }
+
     public boolean isStrongPassword(String password) {
         String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         Pattern pattern = Pattern.compile(passwordRegex);
@@ -36,23 +40,35 @@ public class User {
         System.out.print("enter your email:");
         Scanner input = new Scanner(System.in);
         email = input.nextLine();
-        while(!isValidEmail(email)){
+        while (!isValidEmail(email)) {
             System.out.print("enter a valid email:");
             email = input.nextLine();
         }
-        if(isFoundEmail(email)){
+        if (isFoundEmail(email)) {
             System.out.println("this email is already registered");
             System.exit(0);
         }
         System.out.print("enter your password:");
         password = input.nextLine();
-        while(!isStrongPassword(password)){
+        while (!isStrongPassword(password)) {
             System.out.print("enter a strong password:");
             password = input.nextLine();
         }
         System.out.print("enter your address:");
         address = input.nextLine();
-        System.out.println("Your account has been created successfully");
+
+        OTP userOTP = new OTP();
+        String OTP = userOTP.generateOTP();
+        userOTP.sendOTP(email, OTP);
+        System.out.print("To verify your email enter the OTP you received: ");
+        String enteredOTP = input.nextLine();
+        if (enteredOTP.equals(OTP)) {
+            System.out.println("Your account has been created successfully");
+        } else {
+            System.out.println("Invalid OTP, try again later.");
+            System.exit(0);
+        }
+
         //store user data in database
         try {
             FileWriter writer = new FileWriter("Users.txt", true);
@@ -62,9 +78,10 @@ public class User {
             e.printStackTrace();
         }
     }
+
     public void login() throws IOException {
         boolean isFoundEmail = false;
-        while(!isFoundEmail) {
+        while (!isFoundEmail) {
             System.out.print("enter your email:");
             Scanner input = new Scanner(System.in);
             email = input.nextLine();
@@ -76,7 +93,7 @@ public class User {
                     isFoundEmail = true;
                     String storedPassword = bufferedReader.readLine();
                     int numberOfTries = 3;
-                    while(numberOfTries-- >0) {
+                    while (numberOfTries-- > 0) {
                         System.out.print("enter your password:");
                         password = input.nextLine();
 
@@ -86,9 +103,9 @@ public class User {
                             System.out.println("You have logged in successfully");
                             return;
                         }
-                        if(numberOfTries != 0) {
+                        if (numberOfTries != 0) {
                             System.out.println("Wrong password. Please try again.");
-                        }else{
+                        } else {
                             System.out.println("You have entered wrong password 3 times. Please try again later.");
                             System.exit(0);
                         }
@@ -100,7 +117,7 @@ public class User {
             }
             System.out.println("Email not found. Press 1 to Try again or 0 to Exit.");
             String choice = input.nextLine();
-            if(!choice.equals("1"))System.exit(0);
+            if (!choice.equals("1")) System.exit(0);
             bufferedReader.close();
         }
 
