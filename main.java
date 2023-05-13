bf823b466640d7d11823e2bf6a06e811bfc0020c
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2023-05-13 18:40:55 +0300

    Update main.java

--- main.java
+++ main.java
@@ -16,28 +16,33 @@ public class main {
      * the work of Toffee Store program.
      */
     public static void main(String[] args) throws IOException {
-        System.out.println("1- Register");
-        System.out.println("2- Login");
-        System.out.println("3- Display Catalog");
-        System.out.print("enter your choice:");
+        System.out.println("1- Register ");
+        System.out.println("2- Login ");
+        System.out.println("3- Display Catalog ");
+        System.out.print("enter your choice: ");
 
         Scanner input = new Scanner(System.in);
         int choice = input.nextInt();
 
         if (choice == 1) {
+            
             //register
-            System.out.println("--------------------Register--------------------");
+            System.out.println("--------------------Register-------------------- ");
             User register = new User();
             register.register();
+            
         } else if (choice == 2) {
             //login and make order and payment
             MakeOrder makeorder = new MakeOrder();
             makeorder.makeOrder();
+            
         } else if (choice == 3) {
             Catalog catalog = new Catalog();
             catalog.displayCatalog();
+            
         } else {
             System.out.println("wrong choice");
+            
         }
     }
 }
